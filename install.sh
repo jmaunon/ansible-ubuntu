71d55181b024caad5ba1d7802ea0c1ff88bfa9b2
jmaunon <juanmixp@gmail.com>
2020-08-02 18:02:06 +0200

    Mayor modifications

--- install.sh
+++ install.sh
@@ -2,18 +2,7 @@
 
 ###################
 # Install ansible #
-if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    echo "Adding Ansible PPA"
-    sudo apt-add-repository ppa:ansible/ansible -y
-fi
-
-if ! hash ansible >/dev/null 2>&1; then
-    echo "Installing Ansible..."
-    sudo apt-get update
-    sudo apt-get install software-properties-common ansible git python-apt -y
-else
-    echo "Ansible already installed"
-fi
+sudo apt install ansible
 
 #####################################
 # Display real installation process #
